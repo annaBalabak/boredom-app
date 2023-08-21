aec3f4775403e9aa5874f28716f799afd02f144f
annaBalabak <annabalabak15@gmail.com>
2023-08-21 19:05:35 +0200

    changed protocol

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ getAdvice()
   }
  
     const getAdvice = async()=>{
-      const responce = await fetch("http://www.boredapi.com/api/activity/");
+      const responce = await fetch("https://www.boredapi.com/api/activity/");
       const data = await responce.json();
       setAdvice(data.activity)
     }
