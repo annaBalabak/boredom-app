2a5ed0c1f0c5c61337c48f020f7a64a694d7e6ec
annaBalabak <annabalabak15@gmail.com>
2023-08-21 14:26:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState('');
+  const [visible, setVisible] = useState(false)
+
+  const showAdvise = ()=>{
+setVisible(true)
+getAdvice()
+  }
+ 
+    const getAdvice = async()=>{
+      const responce = await fetch("http://www.boredapi.com/api/activity/");
+      const data = await responce.json();
+      setAdvice(data.activity)
+    }
+
+    useEffect(()=>{
+      getAdvice();
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>Are you bored?</h1>
+      <h2>Find out what can you do...</h2>
+      
+      
+      <button onClick={showAdvise}>Get Idea</button>
+      {visible && <p>{advice}</p>}
+    </div>
     </div>
   );
 }
